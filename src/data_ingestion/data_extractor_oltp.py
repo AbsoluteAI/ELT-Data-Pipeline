9c89a81c71bcd7068f79312f689cd2ed189e4e61
AbsoluteAI <aldodd93@gmail.com>
2026-04-05 21:32:45 -0500

    feat: switched to mysql db for oltp, removed polling capabilities for use in other script project docs: updated requirements

--- src/data_ingestion/data_extractor_oltp.py
+++ src/data_ingestion/data_extractor_oltp.py
@@ -21,35 +21,48 @@ def fetch_data():
     try:
         csv_files = list(folder_path.glob("*.csv"))
 
+        print(f"\nSaving csv data to new table:")
+
         try:
+            count = 0
             for file in csv_files:
                 file_name = file.name
-                print(f"\nSaving csv data to new table: {file_name.removesuffix(".csv")}")
+                print(file_name.removesuffix(".csv"))
                 df = pd.read_csv(file)
                 engine = create_engine(f"mysql+mysqlconnector://{mysql_username}:{mysql_password}@{mysql_host}/{mysql_database}")
-                df.to_sql(file_name.removesuffix(".csv"), con=engine, if_exists='replace', index=False)
+                df.to_sql(file_name.removesuffix(".csv"), con=engine, if_exists='replace', index=False, chunksize=10000)
+                count += 1
+
+            print(f"\nSuccessfully wrote {count} files to database")
+
         except Exception as e:
             print(f"Unable to write data to database: {e}")
 
     except Exception as e:
         print(f"Could not find any csv files: {e}")
 
-
-
 def mysql_db_connect():
     global mysql_username, mysql_password, mysql_host, mysql_database
+
+
+    config = {
+        "user": mysql_username,
+        "password": mysql_password,
+        "host": mysql_host,
+        "raise_on_warnings": True
+    }
 
     print(f"Connecting to mysql DB: {mysql_database}")
 
     try:
-        conn = mysql.connector.connect(
-            host=mysql_host,
-            user=mysql_username,
-            password=mysql_password,
-            database=mysql_database
-        )
+        conn = mysql.connector.connect(**config, use_pure=True)
 
-        print("Connected to MySQL DB")
+        if conn.is_connected:
+            print("Connected to MySQL DB")
+            cursor = conn.cursor()
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {mysql_database}")
+        else:
+            return False
 
     except Exception as e:
         print(f"Could not connect to MYSQL DB: {e}")
--- test/test_mysqldb_connection.py
+++ test/test_mysqldb_connection.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import mysql.connector
+from mysql.connector.plugins import caching_sha2_password
 
 # global variables
 load_dotenv()
@@ -12,17 +13,24 @@ mysql_database = os.getenv("MYSQL_DATABASE")
 def mysql_db_connect():
     global mysql_username, mysql_password, mysql_host, mysql_database
 
+
+    config = {
+        "user": mysql_username,
+        "password": mysql_password,
+        "host": mysql_host,
+        "database": mysql_database,
+        "raise_on_warnings": True
+    }
+
     print(f"Connecting to mysql DB: {mysql_database}")
 
     try:
-        conn = mysql.connector.connect(
-            host=mysql_host,
-            user=mysql_username,
-            password=mysql_password,
-            database=mysql_database
-        )
+        conn = mysql.connector.connect(**config, use_pure=True)
 
-        print("Connected to MySQL DB")
+        if conn.is_connected:
+            print("Connected to MySQL DB")
+            print("Closing connection")
+            conn.close()
 
     except Exception as e:
         print(f"Could not connect to MYSQL DB: {e}")
